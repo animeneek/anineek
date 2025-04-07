dbe9e47359c184c351a388d0fd0cb04a6d4e4c35
animeneek <imemyanime@gmail.com>
2025-04-07 15:25:18 +0400

    Update index.js

--- index.js
+++ index.js
@@ -34,21 +34,24 @@ function applyFiltersAndSearch(query) {
     const genreFilter = document.getElementById("genreFilter").selectedOptions;
     const themeFilter = document.getElementById("themeFilter").selectedOptions;
     const demographicFilter = document.getElementById("demographicFilter").selectedOptions;
-    const yearFilter = document.getElementById("yearFilter").value;
-    const typeFilter = document.getElementById("typeFilter").value;
-    const statusFilter = document.getElementById("statusFilter").value;
+    const yearFilter = document.getElementById("yearFilter").selectedOptions;
+    const typeFilter = document.getElementById("typeFilter").selectedOptions;
+    const statusFilter = document.getElementById("statusFilter").selectedOptions;
 
     const genres = Array.from(genreFilter).map(option => option.value).join(",");
     const themes = Array.from(themeFilter).map(option => option.value).join(",");
     const demographics = Array.from(demographicFilter).map(option => option.value).join(",");
+    const years = Array.from(yearFilter).map(option => option.value).join(",");
+    const types = Array.from(typeFilter).map(option => option.value).join(",");
+    const statuses = Array.from(statusFilter).map(option => option.value).join(",");
 
     let searchUrl = `search.html?q=${query}`;
     if (genres) searchUrl += `&genre=${genres}`;
     if (themes) searchUrl += `&theme=${themes}`;
     if (demographics) searchUrl += `&demographic=${demographics}`;
-    if (yearFilter) searchUrl += `&year=${yearFilter}`;
-    if (typeFilter) searchUrl += `&type=${typeFilter}`;
-    if (statusFilter) searchUrl += `&status=${statusFilter}`;
+    if (years) searchUrl += `&year=${years}`;
+    if (types) searchUrl += `&type=${types}`;
+    if (statuses) searchUrl += `&status=${statuses}`;
 
     window.location.href = searchUrl;
 }
